c8c1b754ae1345d5fdfcc66775c238edc9c574ce
Michelle Garcia <thelionpear@gmail.com>
2018-10-08 16:13:44 -0600

    board_game controller

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,13 +1,48 @@
 class BoardGamesController < ApplicationController
   def index
+    render json: Board_Game.all
   end
 
   def show
+    render json: @board_games
   end
 
   def new
+    board_game = Board_Game.new 
+    if board_game.save
+      render json: board_game 
+    else
+      render json: board_game.errors 422
+    end 
   end
 
-  def edit
+  def update
+    if @board_game.update(board_game_params)
+      render json: @board_game 
+    else 
+      render_error(@board_game)
+    end 
   end
+
+  def destroy 
+    @board_game.destroy 
+  end 
+
+  private 
+
+  def set_board_game 
+    @board_game = Board_Game.find(params[:id])
+  end 
+
+  def board_game_params
+    params.require(:board_game).permit(
+    :title,
+    :min_players,
+    :max_players,
+    :base_game,
+    :time_needed,
+    :company 
+    )
+  end 
+
 end
--- db/migrate/20180919182629_create_board_games.rb
+++ db/migrate/20180919182629_create_board_games.rb
@@ -3,17 +3,25 @@ class CreateBoardGames < ActiveRecord::Migration[5.2]
     create_table :board_games do |t|
       t.string :title
       t.string :expansions
+      #removed
       t.string :creator
       t.integer :min_players
       t.integer :max_players
+      #removed
       t.string :play_history
       t.integer :time_needed
+      #removed
       t.boolean :played_this_year
+      #removed
       t.boolean :played_this_month
+      #removed
       t.boolean :played
+      #removed
       t.string :edition
+      #removed
       t.string :year_released
       t.string :company
+      #removed 
       t.string :awards
 
       t.timestamps
