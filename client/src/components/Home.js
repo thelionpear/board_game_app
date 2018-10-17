d68f36d8c8d1e189d8347bd8edc825a7ec5983a5
Michelle Garcia <thelionpear@gmail.com>
2018-10-17 10:27:49 -0600

    home page progress, stuck on bug

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 
 class Home extends Component {
+  
+  homePage = () => {
+    debugger 
+    const { user } = this.props;
+    if (user.id) {
+      return (
+        <div>
+          <div>Games</div>
+          <div>Sessions</div> 
+          <div>Friends</div>
+          <div>Tools</div> 
+          <div>Help</div>
+          <div>Settings</div>
+          <div>Profile</div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <Header as='h1' textAlign='center'>Welcome to Board Game Tracker</Header>
+        <Header as='h2' textAlign='center'>Please Log In or Register</Header>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <Header as='h1' textAlign='center'>Welcome to Board Game Tracker.</Header>
+      <div>
+        { this.homePage() }
+      </div>
     );
   }
 }
+
+// const mapStateToProps = state => {
+//   return { user: state.user };
+// };
 
 export default Home;
